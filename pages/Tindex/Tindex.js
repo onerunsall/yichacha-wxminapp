8e88bc848052b8ae9866bd967e6b17ef23e6ca61
xuxk <762933661@qq.com>
2018-10-24 09:13:43 +0800

    1

--- pages/Tindex/Tindex.js
+++ pages/Tindex/Tindex.js
@@ -8,6 +8,9 @@ Page({
     list: [],
     adverCover: '',
     hrefs: '',
+    lastRowId:'',
+    domain:'',
+    septum:''
   },
 
   /**
@@ -18,6 +21,9 @@ Page({
     var that = this;
     var domain = 'https://dev.njshangka.com';
     wx.setStorageSync('domain', domain);
+    that.setData({
+      domain: domain
+    })
     wx.request({
       url: domain + '/yichaxun/u/homeadlist',
       header: {
@@ -27,11 +33,16 @@ Page({
       success: function(res) {
         wx.hideToast()
         if (res.data.code == 0) {
-          console.log(res.data.data.items.length)
           var homeAdPic = '';
           for (var i = 0; i < res.data.data.items.length; i++) {
             homeAdPic = res.data.data.items[i].homeAdPic;
-            console.log(homeAdPic)
+            var str = homeAdPic;
+            var sear = new RegExp('com');
+            if (sear.test(str)) {
+
+            } else {
+              homeAdPic = domain + homeAdPic;
+            }
             if (!res.data.data.items[i].homeAdUrl) {
               that.setData({
                 hrefs: ''
@@ -39,15 +50,14 @@ Page({
             } else {
               var _html_ = res.data.data.items[i].homeAdUrl;
               that.setData({
-                // hrefs: _html_,
+                hrefs: _html_,
               })
             }
           }
 
           that.setData({
-            adverCover: 'http://passion.njshangka.com/oss/yichaxun/201706010932055979500.jpg',
-            // adverCover: homeAdPic,
-            hrefs: 'https://passion.njshangka.com/yichaxun/weixin/newsDetails.html'
+            adverCover: homeAdPic,
+            // hrefs: 'https://passion.njshangka.com/yichaxun/weixin/newsDetails.html'
           })
         } else {
           wx.showModal({
@@ -56,8 +66,66 @@ Page({
         }
       }
     })
-  },
 
+
+    that.lastPage('','')
+  },
+  lastPage: function (lastRowId, septum) {
+    var that = this;
+
+
+    wx.request({
+      url: that.data.domain+'/yichaxun/data/list',
+      method: 'post',
+      data: {
+        dataOnHome:1,
+        lastRowId: lastRowId,
+        septum: septum
+      },
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded",
+
+      },
+      success: function (res) {
+
+
+        if (res.data.code == 0) {
+
+          var list = that.data.list;
+          var newlist = list.concat(res.data.data.items)
+          if (res.data.data.items.length == 0) {
+            that.setData({
+              list: newlist,
+            });
+            wx.showToast({
+              title: '数据已全部加载',
+              // icon: 'loading',
+              // duration: 1500
+            })
+          } else {
+            for (var i = 0; i < res.data.data.items.length; i++) {
+              that.setData({
+                lastRowId: res.data.data.items[i].dataId,
+                septum: res.data.data.items[i].dataOnHomeSort,
+              })
+              console.log(that.data.lastRowId)
+            }
+            that.setData({
+              list: newlist,
+            });
+          }
+        }
+
+
+        else if (res.data.code == 20 || res.data.code == 26) {
+          wx.hideToast()
+          wx.navigateTo({
+            url: '../login/login',
+          })
+        }
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -96,10 +164,12 @@ Page({
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
-
+  onReachBottom: function () {
+    var that = this
+    var lastRowId = that.data.lastRowId;
+    var septum = that.data.septum;
+    that.lastPage(lastRowId, septum)
   },
-
   /**
    * 用户点击右上角分享
    */
