6209b1b65e19aaf35365765920ee38617c9b6f3d
xuxk <762933661@qq.com>
2018-10-25 16:56:38 +0800

    2

--- pages/Tindex/Tindex.js
+++ pages/Tindex/Tindex.js
@@ -1,4 +1,5 @@
 // pages/Tindex/Tindex.js
+const app = getApp()
 Page({
 
   /**
@@ -11,9 +12,53 @@ Page({
     lastRowId:'',
     domain:'',
     septum:'',
-    type:1
+    type:1,
+    showModal: false,
+    userInfo: {},
+    code:'',
   },
+  nook: function (e) {
+      this.setData({
+        showModal:false
+      })
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    var that=this
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      showModal: false
+    })
 
+    wx.request({
+      url: that.data.domain + '/yichaxun/u/ycc/ua/wxminapplogin',
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      method: 'post',
+      data: {
+        code: that.data.code,
+        nickname: that.data.userInfo.nickName,
+        avatarUrl: that.data.userInfo.avatarUrl,
+        gender: that.data.userInfo.gender,
+        country: that.data.userInfo.country,
+        province: that.data.userInfo.province,
+        city: that.data.userInfo.city,
+        language: that.data.userInfo.language,
+      },
+      success: function (res) {
+        wx.hideToast()
+        if (res.data.code == 0) {
+          wx.setStorageSync('token', res.data.data.token)
+        } else {
+          wx.showModal({
+            title: res.data.codeMsg
+          })
+        }
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -26,22 +71,92 @@ Page({
       domain: domain
     })
 
-wx.login({
-  success (res) {
-    if (res.code) {
-      //发起网络请求
-      console.log((res.code));
-      // wx.request({
-      //   url: 'https://test.com/onLogin',
-      //   data: {
-      //     code: res.code
-      //   }
-      // })
+    wx.login({
+      success(res) {
+        if (res.code) {
+          that.setData({
+            code: res.code
+          })
+          //发起网络请求
+          console.log((res.code));
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
+
+    wx.getSetting({
+      success(res) {
+        if (!res.authSetting['scope.userInfo']) {
+          that.setData({
+            showModal: true
+          })
+        }
+      }
+    })
+
+
+
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
     } else {
-      console.log('登录失败！' + res.errMsg)
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          // wx.relaunch({
+          //   url: '../Tindex/Tindex',
+          // })
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+          wx.request({
+            url: that.data.domain + '/yichaxun/u/ycc/ua/wxminapplogin',
+            header: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+            method: 'post',
+            data: {
+              code: that.data.code,
+              nickname: that.data.userInfo.nickName,
+              avatarUrl: that.data.userInfo.avatarUrl,
+              gender: that.data.userInfo.gender,
+              country: that.data.userInfo.country,
+              province: that.data.userInfo.province,
+              city: that.data.userInfo.city,
+              language: that.data.userInfo.language,
+            },
+            success: function (res) {
+              wx.hideToast()
+              if (res.data.code == 0) {
+                wx.setStorageSync('token', res.data.data.token)
+              } else {
+                wx.showModal({
+                  title: res.data.codeMsg
+                })
+              }
+            }
+          })
+        }
+      })
     }
-  }
-})
+
+  
+
+
 
 
     wx.request({
